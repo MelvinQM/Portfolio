1f41e1f8a20337e6d593b18255b8e4a3c0e9094c
Melvin Moes <melv5@live.nl>
2025-02-03 19:57:57 +0100

    Basic projects page layout

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,8 +1,26 @@
-export default function Projects() {
+import '../css/Projects.css'
+import projectsData from '../data/projects.json'
+interface Project {
+  id: number,
+  name: string,
+  description: string,
+  skills: string[],
+  projectURL?: string,
+  githubURL?: string
+}
 
+export default function Projects() {
+  const projects: Project[] = projectsData
   return (
     <>
-        Projects
+      <div className="projects-container">
+        {projects.map((project, index) => (
+          <div key={index} className="project">
+            <h2>{project.name}</h2>
+            <p>{project.description}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }

4b39d1f944e13c8985fae423903e6e3258aef8a1
Melvin Moes <melvin.moes@hva.nl>
2025-09-13 02:12:27 +0200

    Improved styling of workexperience

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -10,37 +10,34 @@ export default function About() {
     <section className="box about-section" id="about">
       <Container>
         <Row className='justify-content-center'>
-          <Col xs={12} md={6} xl={8}>
-          <div className='experience-content'>
-            <h1>Work Experience</h1>
-            <p>My previous relevant work experience</p>
-            <div className="experiences-container">
-              {workExperience.slice().sort((x, y) => x.order - y.order).map(x => (
-                <Container className='experience d-flex' key={x.order}>
-                  <div className='flex-shrink-0' style={{ width: '150px' }}>
-                    {Array.isArray(x.date) ? (
-                      <div className='d-flex flex-column' >
-                        {x.date.map((date, index) => (
-                          <div className='me-4 fw-bold' key={index}>{date}</div>
-                        ))}
+          <Col xs={12} md={10} xl={8}>
+            <div className='experience-content'>
+              <h1>Work Experience</h1>
+              <p>My previous relevant work experience</p>
+              <div className="experiences-container">
+                {workExperience.slice().sort((x, y) => x.order - y.order).map(x => (
+                  <Container className='experience d-flex' key={x.order}>
+                    <div className='experience-header'>
+                      <div className='d-flex flex-column fw-bold'>
+                        <div className='text-start'>{x.jobTitle}</div>
+                        <div>{x.companyURL ? (<a className="custom-a" href={x.companyURL}>{x.company}</a>): <>{x.company}</>}</div>
+                        {Array.isArray(x.date) ? (
+                            <div className='d-flex flex-column' >
+                              {x.date.map((date, index) => (
+                                <div className='me-4 experience-date' key={index}>{date}</div>
+                              ))}
+                            </div>
+                          ) : ( <div className='me-4 experience-date'>{x.date}</div>)}
                       </div>
-                    ) : ( <div className='me-4 fw-bold'>{x.date}</div>)}
-                  </div>
-                  <div className='experience-header'>
-                    <div className='d-flex fw-bold'>
-                      <div className='text-start'>{x.jobTitle}</div>
-                      <div>&nbsp;&mdash;&nbsp;</div>
-                      <div>{x.companyURL ? (<a className="custom-a" href={x.companyURL}>{x.company}</a>): <>{x.company}</>}</div>
+                      <div className='text-start mb-2'>{x.description}</div>
+                      <div className='experience-skills'>
+                        {x.skills.map((skill, index) => (<div key={index} className='me-2'>{skill}</div>))}
+                      </div>
                     </div>
-                    <div className='experience-skills'>
-                      {x.skills.map((skill, index) => (<div key={index} className='me-2'>{skill}</div>))}
-                    </div>
-                    <div className='text-start'>{x.description}</div>
-                  </div>
-                </Container>
-              ))}
+                  </Container>
+                ))}
+              </div>
             </div>
-          </div>
           </Col>
         </Row>
       </Container>

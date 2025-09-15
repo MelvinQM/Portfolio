ca4270227de13ba4877a51e6e3233357883b3e93
Melvin Moes <melvin.moes@hva.nl>
2025-09-15 02:48:21 +0200

    Fixed project view reactivity issue

--- frontend/src/components/YoutubeEmbed.tsx
+++ frontend/src/components/YoutubeEmbed.tsx
@@ -1,14 +1,18 @@
+import "./YoutubeEmbed.css";
+
 export default function YouTubeEmbed ({ videoId, className }: {videoId: string, className?: string}) {
     return (
-      <div className={`youtube-container ${className}`}>
-        <iframe
-          width="560"
-          height="315"
-          src={`https://www.youtube.com/embed/${videoId}`}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+      <div className="youtube-embed-wrapper">
+        <div className={`youtube-container ${className}`}>
+          <iframe
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${videoId}`}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
     );
 };
